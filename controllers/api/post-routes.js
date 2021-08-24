d763b34ac4c96da00870446e81dd9de8066c605c
Meghan Bock <meghan.bock@gmail.com>
2021-08-24 02:08:15 -0500

    add upvotes, add comments, and conditionally show/hide based on login status

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -105,12 +105,14 @@ router.post("/", (req, res) => {
 // create vote via post put
 router.put("/upvote", (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then((updatedPostData) => res.json(updatedPostData))
+  if (req.session) {
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+    .then((updatedVoteData) => res.json(updatedVoteData))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
     });
+  }
 });
 
 // update post
