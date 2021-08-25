58844c33ece8e2bb4f49a990408c9a8fe924a939
Meghan Bock <meghan.bock@gmail.com>
2021-08-25 00:41:48 -0500

    set up partials

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const homeRoutes = require('./home-routes');
 
+const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 require('dotenv').config();
 
 const app = express();
@@ -23,7 +24,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
